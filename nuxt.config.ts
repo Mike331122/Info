75e1515f52e3a94cbe31b0c0d9d736c6254d7f8c
Mike331122 <micpel60@wp.pl>
2025-02-02 17:10:41 +0100

    Naprawienie bdu z Netlify v2 -- Pinceau

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 export default defineNuxtConfig({
+  
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
+  modules: ['pinceau'],
+  pinceau: {
+    configFileName: 'tokens.config.ts'
+  },
 
   runtimeConfig: {
     apiNinjaKey: process.env.apiNinjaKey
--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,16 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
-})
+    space: {
+      4: '1rem',
+      6: '1.5rem',
+      7: '1.75rem',
+    },
+    fontSize: {
+      '2xl': '1.5rem',
+    },
+    fontWeight: {
+      bold: '700',
+    }
+  });
+  
