5ae16ee1a36f97e85ca3a07693b3b78cc8d060e6
Mike331122 <micpel60@wp.pl>
2025-02-02 19:08:25 +0100

    Naprawienie bdu z Netlify v5 -- Zmiana nuxt.config.ts - dodanie wymuszenia sser

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 export default defineNuxtConfig({
-  
+  ssr: true,
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
-  modules: ['pinceau'],
-  pinceau: {
-    configFileName: 'tokens.config.ts'
-  },
 
   runtimeConfig: {
-    apiNinjaKey: process.env.apiNinjaKey
+    apiNinjaKey: process.env.apiNinjaKey,
+    public: {
+      NODE_ENV: process.env.NODE_ENV
+    }
+    
   },
   compatibilityDate: '2024-10-24'
 })
